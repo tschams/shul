8792ffa75614eab55d346476c6fb5319ffef203b
tschams <tschams2@gmail.com>
2021-03-07 14:33:42 -0500

    react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,107 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-// props
-// hooks
-// render props
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
 
-const App: React.FC = () => {
-    return <div>yoyyyyyyyyyrrrr</div>;
-};
+export default function BasicExample() {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">HOME</Link>
+                    </li>
+                    <li>
+                        <Link to="/minyanim">MINYANIM</Link>
+                    </li>
+                    <li>
+                        <Link to="/shiurim">SHIURIM</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">ABOUT US</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">CONTACT US</Link>
+                    </li>
+                </ul>
 
-export default App;
+                <hr />
+
+                {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route exact path="/minyanim">
+                        <Minyanim />
+                    </Route>
+                    <Route exact path="/shiurim">
+                        <Shiurim />
+                    </Route>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+}
+
+function Minyanim() {
+    return (
+        <div>
+            <h2>Minyanim</h2>
+        </div>
+    );
+}
+
+function Shiurim() {
+    return (
+        <div>
+            <h2>Shiurim</h2>
+        </div>
+    );
+}
+
+function About() {
+    return (
+        <div>
+            <h2>About</h2>
+        </div>
+    );
+}
+
+function Contact() {
+    return (
+        <div>
+            <h2>Contact</h2>
+        </div>
+    );
+}
