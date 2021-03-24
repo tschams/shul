a18f25f8a526bbb2e7898b36e849ce6a5573806c
tschams <tschams2@gmail.com>
2021-03-24 16:33:01 -0400

    material ui input using vanilla css

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,42 @@
 import React from "react";
+import styles from "./Login.module.css";
+import clsx from "clsx";
 
 function _Login() {
+  const inputEl = React.useRef<HTMLInputElement>(null);
+  const [input, setInput] = React.useState("");
+
   return (
-    <>
-      <h2>Login</h2>
-    </>
+    <form noValidate autoComplete="off">
+      <div
+        className={styles.inputAndLabelContainer}
+        onClick={() => inputEl.current && inputEl.current.focus()}
+      >
+        <label
+          id="name-label"
+          className={clsx(styles.label, {
+            [styles.unTransitionLabel]: input,
+          })}
+          htmlFor="name"
+        >
+          Name
+        </label>
+        <div
+          className={clsx(styles.inputContainer, styles.inputContainerHover)}
+        >
+          <input
+            ref={inputEl}
+            aria-invalid="false"
+            className={styles.input}
+            type="text"
+            id="name"
+            name="name"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+      </div>
+    </form>
   );
 }
 
