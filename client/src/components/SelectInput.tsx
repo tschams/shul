86309e5697f4bc5a0386b799003428168a518096
tschams <tschams2@gmail.com>
2021-04-12 18:14:52 -0400

    select input

--- client/src/components/SelectInput.tsx
+++ client/src/components/SelectInput.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import ArrowDropDownIcon from "../icons/ArrowDropDownIcon.svg";
+import Select from "react-select";
+
+const options = ["chocolate", "strawberry", "vanilla"];
+
+const optionsForSelect = options.map((option) => ({
+  value: option,
+  label: option.charAt(0).toUpperCase() + option.slice(1),
+}));
+
+type selected = { value: string; label: string } | null;
 
 function _SelectInput() {
+  const [selectedOption, setSelectedOption] = React.useState<selected>(null);
+
+  const handleChange = React.useCallback((selectedOption: selected) => {
+    setSelectedOption(selectedOption);
+    console.log(`Option selected:`, selectedOption);
+  }, []);
   return (
-    <>
-      <img src={ArrowDropDownIcon} alt="" />
-    </>
+    <Select
+      value={selectedOption}
+      onChange={handleChange}
+      options={optionsForSelect}
+    />
   );
 }
 
