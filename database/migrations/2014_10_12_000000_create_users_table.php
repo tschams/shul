abb7b8f73e2c0b4594f5376f3320d60f9425e657
tschams <tschams2@gmail.com>
2021-04-18 23:14:59 -0400

    prettier format for php and json

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,42 +6,42 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('title')->nullable();
-            $table->string('hebrew_name')->nullable();
-            $table->string('mother_hebrew_name')->nullable();
-            $table->string('father_hebrew_name')->nullable();
-            $table->enum('tribe', ['Cohen', 'Levi', 'Yisroel'])->nullable();
-            $table->enum('gender', ['Male', 'Female'])->nullable();
-            $table->string('phone_number')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->date('date_of_birth')->nullable();
-            $table->date('hebrew_date_of_birth')->nullable();
-            $table->boolean('admin');
-            $table->timestamps();
-            $table->softDeletes();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->string('title')->nullable();
+      $table->string('hebrew_name')->nullable();
+      $table->string('mother_hebrew_name')->nullable();
+      $table->string('father_hebrew_name')->nullable();
+      $table->enum('tribe', ['Cohen', 'Levi', 'Yisroel'])->nullable();
+      $table->enum('gender', ['Male', 'Female'])->nullable();
+      $table->string('phone_number')->nullable();
+      $table->string('email')->unique();
+      $table->timestamp('email_verified_at')->nullable();
+      $table->string('password');
+      $table->rememberToken();
+      $table->date('date_of_birth')->nullable();
+      $table->date('hebrew_date_of_birth')->nullable();
+      $table->boolean('admin');
+      $table->timestamps();
+      $table->softDeletes();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('users');
+  }
 }
