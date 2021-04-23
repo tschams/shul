4d9487ef2c86cc5ca6c52f48ce8eef10bfdf1faf
tschams <tschams2@gmail.com>
2021-04-22 23:11:12 -0400

    login modal start, working to close if outside click

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink,
+  NavLink
 } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
@@ -14,6 +14,7 @@ import { Login } from "./pages/Login";
 import logo from "./assets/images/logo.jpg";
 import styles from "./App.module.css";
 import { Button } from "./components/Button";
+import clsx from "clsx";
 
 // This site has 3 pages, all of which are rendered
 // dynamically in the browser (not server rendered).
@@ -25,6 +26,25 @@ import { Button } from "./components/Button";
 // work properly.
 
 export default function App() {
+  const ref = React.useRef<HTMLSpanElement>(null);
+  const [modal, setModal] = React.useState(false);
+
+  const handleClick = React.useCallback((e: MouseEvent) => {
+    if (ref.current?.contains(e.target as Node)) {
+      // inside click
+      return;
+    }
+    // outside click
+    setModal(false);
+  }, []);
+
+  React.useEffect(() => {
+    document.addEventListener("mousedown", e => handleClick(e));
+    return () => {
+      document.removeEventListener("mousedown", e => handleClick(e));
+    };
+  }, [handleClick]);
+
   return (
     <>
       <Router>
@@ -78,15 +98,22 @@ export default function App() {
               <span>CONTACT US</span>
             </NavLink>
           </li>
-          <li className={styles.navLinkContainer}>
-            <NavLink
-              className={styles.navLink}
-              activeClassName="navLinkSelected"
-              to="/login"
-            >
-              <span>LOGIN</span>
-            </NavLink>
-          </li>
+          {!modal ? (
+            <li className={styles.navLinkContainer}>
+              <span
+                onClick={() => setModal(!modal)}
+                className={clsx(styles.navLink, styles.login)}
+              >
+                <span>
+                  LOGIN<strong>/</strong>REGISTER
+                </span>
+              </span>
+            </li>
+          ) : (
+            <span ref={ref}>
+              <Login />
+            </span>
+          )}
           <li>
             <Button>Donate</Button>
           </li>
@@ -114,9 +141,6 @@ export default function App() {
           </Route>
           <Route path="/contact">
             <Contact />
-          </Route>
-          <Route path="/login">
-            <Login />
           </Route>
         </Switch>
       </Router>
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,41 +1,22 @@
 import React from "react";
-import { HebrewKeyboard } from "../components/HebrewKeyboard";
 import { Input } from "../components/Input";
-import { RadioButtons } from "../components/RadioButtons";
-import { SelectInput } from "../components/SelectInput";
 import { objectArrayToObject } from "../utils";
-import ReCAPTCHA from "react-google-recaptcha";
 
 function _Login() {
   const inputNames = ["firstName", "lastName", "hebrewName"];
   const [inputs, setInputs] = React.useState(() => {
-    const inputArray = inputNames.map((e) => ({ [e]: "" }));
+    const inputArray = inputNames.map(e => ({ [e]: "" }));
     return objectArrayToObject(inputArray);
   });
   const handleChange = React.useCallback(
     (value: object) => setInputs({ ...inputs, ...value }),
     [inputs]
   );
-  function onChange(value: string | null) {
-    console.log("Captcha value:", value);
-  }
 
   return (
     <form noValidate autoComplete="off">
-      <ReCAPTCHA
-        sitekey={process.env.MIX_RECAPTCHA_SITE_KEY as string}
-        onChange={(v) => onChange(v)}
-      />
-      <SelectInput />
-      <RadioButtons />
       {inputNames.map((e, i) => (
-        <Input
-          key={i}
-          value={inputs[e]}
-          name={e}
-          onNameChange={handleChange}
-          children={e === "hebrewName" && <HebrewKeyboard />}
-        />
+        <Input key={i} value={inputs[e]} name={e} onNameChange={handleChange} />
       ))}
     </form>
   );
