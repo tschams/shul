c1bfea40b4b91a2b630e860c27f8d9b766f075ec
tschams <tschams2@gmail.com>
2021-04-23 12:01:52 -0400

    removed router switch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
@@ -16,15 +11,6 @@ import styles from "./App.module.css";
 import { Button } from "./components/Button";
 import clsx from "clsx";
 import { useOnMount } from "../customHooks";
-
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
 
 export default function App() {
   const ref = React.useRef<HTMLSpanElement>(null);
@@ -120,30 +106,21 @@ export default function App() {
           </li>
         </ul>
 
-        {/*
-            A <Switch> looks through all its children <Route>
-            elements and renders the first one whose path
-            matches the current URL. Use a <Switch> any time
-            you have multiple routes, but you want only one
-            of them to render at a time
-          */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/minyanim">
-            <Minyanim />
-          </Route>
-          <Route path="/shiurim">
-            <Shiurim />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/minyanim">
+          <Minyanim />
+        </Route>
+        <Route path="/shiurim">
+          <Shiurim />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
       </Router>
       <footer>&copy; 2021 All rights reserved</footer>
     </>
