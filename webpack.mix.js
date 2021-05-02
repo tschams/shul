6c8decd951951ad8d1232e7a32fee337a926ffda
tschams <tschams2@gmail.com>
2021-05-01 23:53:36 -0400

    laravel mix remove .react

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ const path = require('path');
 
 mix
   .js("resources/js/app.js", "public/js")
-  .react()
+  // .react()
   // .sass("resources/sass/app.scss", "public/css")
   // .postCss("resources/css/app.css", "public/css", [
   //   //
